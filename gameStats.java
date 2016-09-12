ba64d8033f70d126218a2e929480c368d539ade2
Brandon <brandongarvey@Brandons-MacBook-Air.local>
2016-09-12 18:08:14 -0400

    Main method

---
+++ gameStats.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/**
+ * 
+ */
+public class gameStats {
+  
+  public static void main(String[] args){
+    
+    String getName; //name of item
+    int getWeight;  //weight of item
+    String examine; //prints name and weight of item
+    int getDamage;  //weapon damage
+    int getDefense; //armor defense
+    int getNutrition; //food nutrition
+    int getQuantity;  //amount of food
+
+  }
+    
+
